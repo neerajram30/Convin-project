f9b572b12d88764c900485481ff51b6f6c9f9501
neerajram30 <neerajramachandran30@gmail.com>
2022-06-11 19:57:36 +0530

    done some finishing works

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,15 +3,24 @@ import { useDispatch, useSelector } from 'react-redux'
 import { fetchAsyncUsers, getAllUsers } from '../../store/Users/usersSlice';
 import axios from 'axios'
 import './Home.css'
+import { RotatingSquare } from 'react-loader-spinner'
+import { ThreeCircles} from 'react-loader-spinner'
+import Fade from 'react-reveal/Fade';
 
 
 function Home() {
   const dispach = useDispatch();
   const [user,setUser] =useState([]) 
   const [isUser,setIsUser] =useState(false)
+  const [loading,setLoading] = useState(false);
+  const [cardloading,setCardLoading] = useState(false);
  
   useEffect(() => {
     dispach(fetchAsyncUsers())
+
+    setTimeout(() => {
+      setLoading(true);
+    }, 4000);
   },[dispach])
   
   const users = useSelector(getAllUsers);
@@ -19,11 +28,12 @@ function Home() {
   // console.log(response);
 
   function handleClick(id){
-
+      setCardLoading(true)
     axios.get(`https://reqres.in/api/users/${id}`)
   .then((response)=> {
     setUser(response.data.data)
     setIsUser(true);
+    setCardLoading(false)
     // console.log(response.data.data);
   })
   .catch((error)=> {
@@ -34,24 +44,47 @@ function Home() {
   console.log(user);
 
   return (
+    <div>
+{!loading ?
+<span className='loader'>
+<RotatingSquare ariaLabel="rotating-square" visible={true} color="white" />
+</span>:
+
+
     <div className='homeContainer'>
       <div className='btnOuter'>
       <div className='btnContainer'>
         {response && 
         response.map((user,i)=>
+        <Fade>
         <div key={i}>
           <button onClick={()=>{handleClick(user.id)}} className="btns">{user.id}</button>
         </div>  
+        </Fade>
         )
       }
       </div>
       </div>
       <div className='cardContainer'>
-       { isUser?
+
+       {cardloading ?
+       <span className='cardloading'>
+         <ThreeCircles
+  color="red"
+  width={20}
+  ariaLabel="three-circles-rotating"
+/>
+       </span>:
+       
+       
+       isUser?
        
        
        user &&
-        (<div className='usercard'>
+        (
+        <Fade>
+
+        <div className='usercard'>
          <div className="imgContainer">
        <img src={user.avatar} alt='avatar' />
          </div>
@@ -60,8 +93,12 @@ function Home() {
        <h3>{user.first_name} {user.last_name}</h3>
        <p>{user.email}</p>
          </div>
-       </div>):
+       </div>
+        </Fade>
+       ):
        (
+         <Fade>
+           
         <div className='usercard'>
          <div className="imgContainer">
          </div>
@@ -69,12 +106,14 @@ function Home() {
        <p>Click any button</p>
          </div>
        </div>
+         </Fade>
        )
-    
+       
       }
       </div> 
 
-    </div>
+    </div>}
+      </div>
   )
 }
 
